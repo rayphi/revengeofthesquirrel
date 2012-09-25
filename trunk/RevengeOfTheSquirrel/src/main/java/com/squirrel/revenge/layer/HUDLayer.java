1d134901f6f92d4c0d1b2b74a6d09158284aae68
ALuedtke <ALuedtke@172.16.98.52>
2012-09-25 13:54:48 +0200

    Score eingebaut

--- trunk/RevengeOfTheSquirrel/src/main/java/com/squirrel/revenge/layer/HUDLayer.java
+++ trunk/RevengeOfTheSquirrel/src/main/java/com/squirrel/revenge/layer/HUDLayer.java
@@ -2,8 +2,11 @@ package com.squirrel.revenge.layer;
 
 import java.awt.Point;
 
+import com.squirrel.engine.game.Configuration;
 import com.squirrel.engine.gameobject.impl.TextualGameObject;
 import com.squirrel.engine.scene.impl.LayerImpl;
+import com.squirrel.engine.utils.ApplicationUtils;
+import com.squirrel.revenge.score.Score;
 
 public class HUDLayer extends LayerImpl {
 	
@@ -13,6 +16,10 @@ public class HUDLayer extends LayerImpl {
 		
 		if (gm.isDebug())
 			initDebugHUD();
+		
+		InitScoreHUD();
+		
+		// Hinzufügen des Score
 	}
 
 	private void initDebugHUD() {
@@ -24,6 +31,22 @@ public class HUDLayer extends LayerImpl {
 				msg = String.valueOf(ps.getFPS()) + " FPS";
 			}
 		});
+		
 	}
+	
+	private void InitScoreHUD(){
+		Configuration conf = (Configuration) ApplicationUtils.getInstance().getBean("configuration");
+				
+		// Score wird im Bild 90% vom linken Rand angezeigt		
+		int posX =	(conf.getScreenWidth() / 100) * 90;
+		// Score wird im Bild 5% vom oberen Rand angezeigt
+		int posY =  (conf.getScreenHeight() / 100) * 5;
+		
+		Score score = new Score();
+		addGameObject(new TextualGameObject("score", new Point(posX,posY), String.valueOf(score.getScore()) , this));
+		
+	}
+	
+	
 
 }
